1618bb73397ad0ee7835d9ed03bf2d9bad281fa9
shiqstone <shiqstone@gmail.com>
2020-08-11 11:32:22 +0800

    refactor: 1 calc md5 with different method for small & big file 2 provide options use file or memory as intermediate result 3 add debug option

--- check_duplica.go
+++ check_duplica.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"crypto/md5"
 	"encoding/hex"
 	"errors"
@@ -9,8 +10,13 @@ import (
 	"io"
 	"io/ioutil"
 	"log"
+	"math"
+	"net/http"
+	_ "net/http/pprof"
 	"os"
 	"path/filepath"
+	"strconv"
+	"strings"
 	"sync"
 	"time"
 )
@@ -18,6 +24,11 @@ import (
 var file *os.File
 var skip int64
 var ignoreErr bool
+var tvType string
+var tvFile *os.File
+
+const filechunk = 8192
+const bigFileSize = 512 * 1024 * 1024
 
 func init() {
 	flag.Usage = usage
@@ -28,14 +39,21 @@ func main() {
 	var help = flag.Bool("h", false, "this help")
 	var dirname = flag.String("p", "", "the directory contains all the files")
 	var limit = flag.Int("m", 10, "limit the max files to caclulate.")
-	flag.Int64Var(&skip, "skip", 0, "skip file size small than set sizes, unit KB")
 	var output = flag.String("o", "", "the path of output check result, not save when set empty")
+	var debug = flag.Bool("debug", false, "is run debug mode")
+	flag.Int64Var(&skip, "skip", 0, "skip file size small than set sizes, unit KB")
 	flag.BoolVar(&ignoreErr, "i", true, "ignore filepath permit errors and skip")
+	flag.StringVar(&tvType, "tvt", "mem", "temp value type, file/mem default mem")
 	flag.Parse()
 
 	if *help {
 		flag.Usage()
 		return
+	}
+	if *debug {
+		go func() {
+			log.Println(http.ListenAndServe("0.0.0.0:10000", nil))
+		}()
 	}
 
 	if len(*dirname) == 0 {
@@ -54,6 +72,21 @@ func main() {
 		if err != nil {
 			log.Fatalln("fail to create output file!")
 		}
+		defer file.Close()
+	}
+	if tvType == "file" {
+		var err error
+		tvFile, err = ioutil.TempFile("./", "returnValue-")
+		if err != nil {
+			log.Fatal("fail to create temp value file", err)
+		}
+		defer func() {
+			tvFile.Close()
+			err := os.Remove(tvFile.Name())
+			if err != nil {
+				log.Fatal(err.Error())
+			}
+		}()
 	}
 
 	skip *= 1024
@@ -65,27 +98,59 @@ func main() {
 	}
 
 	md5map := make(map[string]([]string), 0)
-	for path, item := range result {
-		md5value := item["md5"].([md5.Size]byte)
-
-		var ps []string
-		md5str := hex.EncodeToString(md5value[:])
-		if eps, ok := md5map[md5str]; ok {
-			ps = eps
+	sizemap := make(map[string]int, 0)
+	if tvType == "file" {
+		f, err := os.Open(tvFile.Name())
+		if err != nil {
+			fmt.Println(err.Error())
+			return
 		}
-		ps = append(ps, path)
-		md5map[md5str] = ps
+		defer f.Close()
+		br := bufio.NewReader(f)
+		for {
+			lines, _, err := br.ReadLine()
+			if err == io.EOF {
+				break
+			}
+			items := strings.Split(string(lines), "||")
+			path := items[0]
+			md5str := items[1]
+			var ps []string
+			if eps, ok := md5map[md5str]; ok {
+				ps = eps
+			}
+			ps = append(ps, path)
+			md5map[md5str] = ps
+			sizemap[md5str], _ = strconv.Atoi(items[2])
+		}
+	} else {
+		for path, item := range result {
+			md5value := item["md5"].([]byte)
+
+			var ps []string
+			md5str := hex.EncodeToString(md5value)
+			if eps, ok := md5map[md5str]; ok {
+				ps = eps
+			}
+			ps = append(ps, path)
+			md5map[md5str] = ps
+		}
 	}
 
 	cnt := 0
 	gidx := 0
-	for _, ps := range md5map {
+	for md5k, ps := range md5map {
 		if len(ps) > 1 {
 			logOutput("%d probablely duplica files \n", gidx)
 			//exist duplica file
 			for idx, path := range ps {
-				size := result[path]["size"].(int64)
-				logOutput("%d path: %s | size=%d\n", idx, path, size)
+				if tvType == "file" {
+					size := int64(sizemap[md5k])
+					logOutput("%d path: %s | size=%d\n", idx, path, size)
+				} else {
+					size := result[path]["size"].(int64)
+					logOutput("%d path: %s | size=%d\n", idx, path, size)
+				}
 				cnt++
 			}
 			logOutput("-------%s\n", "-")
@@ -118,18 +183,36 @@ func logOutput(f string, v ...interface{}) {
 	}
 }
 
-func Md5SumFile(file string) (value [md5.Size]byte, err error) {
-	data, err := ioutil.ReadFile(file)
+func Md5SumFile(filepath string) (value []byte, err error) {
+	data, err := ioutil.ReadFile(filepath)
 	if err != nil {
 		return
 	}
-	value = md5.Sum(data)
+	sum := md5.Sum(data)
+	value = sum[:]
 	return
+}
+
+func Md5SumBigFile(filepath string, filesize int64) (value []byte, err error) {
+	file, err := os.Open(filepath)
+	if err != nil {
+		return
+	}
+	defer file.Close()
+	blocks := uint64(math.Ceil(float64(filesize) / float64(filechunk)))
+	hash := md5.New()
+	for i := uint64(0); i < blocks; i++ {
+		blocksize := int(math.Min(filechunk, float64(filesize-int64(i*filechunk))))
+		buf := make([]byte, blocksize)
+		file.Read(buf)
+		io.WriteString(hash, string(buf)) // append into the hash
+	}
+	return hash.Sum(nil), err
 }
 
 type result struct {
 	path   string
-	md5Sum [md5.Size]byte
+	md5Sum []byte
 	size   int64
 	err    error
 	pdir   string
@@ -165,7 +248,8 @@ func Md5SumFolder(folder string, limit int) (map[string]map[string]interface{}, 
 				return nil
 			}
 
-			if skip > 0 && info.Size() < skip {
+			size := info.Size()
+			if skip > 0 && size < skip {
 				return nil
 			}
 
@@ -176,14 +260,19 @@ func Md5SumFolder(folder string, limit int) (map[string]map[string]interface{}, 
 
 			wg.Add(1)
 			go func() {
-				data, err := ioutil.ReadFile(path)
-				size := info.Size()
+				//data, err := ioutil.ReadFile(path)
 				dir, _ := filepath.Split(path)
+				var md5v []byte
+				if size > bigFileSize {
+					md5v, err = Md5SumBigFile(path, size)
+				} else {
+					md5v, err = Md5SumFile(path)
+				}
 
 				select {
 				case c <- result{
 					path:   path,
-					md5Sum: md5.Sum(data),
+					md5Sum: md5v, //md5.Sum(data),
 					size:   size,
 					err:    err,
 					pdir:   dir,
@@ -210,7 +299,7 @@ func Md5SumFolder(folder string, limit int) (map[string]map[string]interface{}, 
 			close(c)
 		}()
 	}()
-	var kps = make(map[string]([]string), 0)
+	//var kps = make(map[string]([]string), 0)
 	var dsize int64 = 0
 
 	for r := range c {
@@ -224,19 +313,29 @@ func Md5SumFolder(folder string, limit int) (map[string]map[string]interface{}, 
 			}
 			return nil, r.err
 		}
-		returnValue[r.path] = map[string]interface{}{
-			"md5":  r.md5Sum,
-			"size": r.size,
+		if tvType == "file" {
+			wstring := fmt.Sprintf("%s||%s||%d\n", r.path, hex.EncodeToString(r.md5Sum[:]), r.size)
+			if tvFile != nil {
+				_, err := tvFile.Write([]byte(wstring))
+				if err != nil {
+					log.Fatalln("fail to write temp value file!")
+				}
+			}
+		} else {
+			returnValue[r.path] = map[string]interface{}{
+				"md5":  r.md5Sum,
+				"size": r.size,
+			}
 		}
 
 		dsize += r.size
 
-		var ps []string
-		if eps, ok := kps[r.pdir]; ok {
-			ps = eps
-		}
-		ps = append(ps, r.path)
-		kps[r.pdir] = ps
+		// var ps []string
+		// if eps, ok := kps[r.pdir]; ok {
+		// 	ps = eps
+		// }
+		// ps = append(ps, r.path)
+		// kps[r.pdir] = ps
 	}
 
 	if err := <-errc; err != nil {
